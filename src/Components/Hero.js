f42c5fa2a98bf2d3c156993dc195d5dc5d09004e
skabaza <skabaza46@gmail.com>
2023-01-03 19:56:49 -0500

    fixed typo.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,15 @@
 import classes from './Hero.module.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
 const Hero = () => {
 
+    const navigate = useNavigate();
+
+    const reserveTable = () => {
+
+        navigate('/booking')
+    }
     return (
         <Container className={classes.hero}>
             <Row>
@@ -9,7 +17,7 @@ const Hero = () => {
                     <h1 className={classes.title}>Little Lemon</h1>
                     <h3>Chicago</h3>
                     <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                    <button>Rserve a Table</button>
+                    <button onClick={reserveTable}>Reserve a Table</button>
 
                 </Col>
                 <Col>
