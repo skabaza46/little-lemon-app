760dd042458a0d11694f4712fe2be972ff9c5d80
skabaza <skabaza46@gmail.com>
2023-01-03 21:29:44 -0500

    designed the footer module.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,44 @@
+import { Container, Nav, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import classes from "./Footer.module.css";
+
 const Footer = () => {
 
     return (
-        <footer>
-            <div>
-                Logo
-            </div>
-            <div>
-                Contact Info
-            </div>
-            <div>
-                Links to social-media
-            </div>
-            <div>
-                Doormat navigation
+        <footer className="page-footer font-small blue pt-4">
+            <Container>
+                <Row>
+                    <div className="col-md-6 mt-md-0 mt-3">
+                        <img src={process.env.PUBLIC_URL +"/littlelemon-logo.png"} width={250}/>
+                    </div>
+
+                    <hr className="clearfix w-100 d-md-none pb-0"/>
+
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Contact Info</h5>
+                        <p>
+                            1662 Pringle Drive<br/>
+                            Chicago, IL 60631<br/>
+                            United States<br/>
+
+                            <br/>
+                            <strong>Phone:</strong> +1 312-375-7584<br/><br/>
+                            <strong>Email:</strong> info@littelemon.com
+                        </p>
+                    </div>
+
+                    <div className="col-md-3 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled ">
+                            <li><a href="https://twitter.com/?lang=en">Twitter</a></li>
+                            <li><a href="https://www.facebook.com/">Facebook</a></li>
+                            <li><a href="https://www.instagram.com/">Instagram</a></li>
+                        </ul>
+                    </div>
+                </Row>
+            </Container>
+            <div className="footer-copyright text-center py-3">© 2020 Copyright:
+                <Nav.Link as={Link} to="/">Little Lemon </Nav.Link>
             </div>
         </footer>
     )
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,25 +1,40 @@
 import { Container, Navbar, Nav} from "react-bootstrap";
+import { Link } from "react-router-dom";
 import classes from './Navigation.module.css';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navigation = () => {
 
     return (
-        <Navbar bg="light" variant="lg" className="justify-content-center">
-            <Container>
-            <Navbar.Brand href="/">
-                <img src={process.env.PUBLIC_URL +"/littlelemon-logo.png"} width={250}/>
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+            <Navbar.Brand>
+                <Link to="/">
+                    <img src={process.env.PUBLIC_URL +"/littlelemon-logo.png"} width={250}/>
+                </Link>
             </Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/menu">Menu</Nav.Link>
-                <Nav.Link href="/booking">Reservations</Nav.Link>
-                <Nav.Link href="/order-online">Order online</Nav.Link>
-                <Nav.Link href="/login">Login</Nav.Link>
-            </Nav>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="about">About</Nav.Link>
+                    <Nav.Link as={Link} to="menu">Menu</Nav.Link>
+                    <Nav.Link as={Link} to="booking">Reservations</Nav.Link>
+                    <Nav.Link as={Link} to="order-online">Order online</Nav.Link>
+                    <Nav.Link as={Link} to="login">Login</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
 };
 
 export default Navigation;
+
